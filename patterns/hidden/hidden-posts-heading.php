a20e6e6d8503b9a339ce80a5207a361d9c9467c1
Ga Satrya <gasatrya.dev@gmail.com>
2025-10-16 12:49:26 +0700

    Refactor templates and add hidden posts heading pattern for improved structure and functionality

---
+++ patterns/hidden/hidden-posts-heading.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Posts heading
+ * Slug: sorai/hidden-posts-heading
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide">
+	<?php esc_html_e( 'Posts', 'sorai' ); ?>
+</h1>
+<!-- /wp:heading -->
