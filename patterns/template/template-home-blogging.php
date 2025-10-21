c0b87f0ecfe90302cc4384bfcf572978f5e41fb3
Ga Satrya <gasatrya.dev@gmail.com>
2025-10-21 15:24:24 +0700

    Add template for blogging home with header and footer structure

---
+++ patterns/template/template-home-blogging.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Blogging home default
+ * Slug: sorai/template-home-blogging
+ * Template Types: front-page, index, home
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+<!-- wp:template-part {"slug":"header","area":"header"} /-->
+
+<!-- wp:group {"tagName":"main","metadata":{"name":"Main"},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group">
+	<!-- wp:group {"metadata":{"name":"Container"},"align":"full","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull">
+		<!-- wp:pattern {"slug":"sorai/page-home-blogging"} /-->
+	</div>
+	<!-- /wp:group -->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
