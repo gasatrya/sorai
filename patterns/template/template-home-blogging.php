5a052160c3906094a8a209abc9d769587c1171a2
Ga Satrya <gasatrya.dev@gmail.com>
2025-10-24 12:15:18 +0700

    feat(templates): update home and archive templates; remove deprecated patterns and enhance term description styles

--- patterns/template/template-home-blogging.php
+++ patterns/template/template-home-blogging.php
@@ -14,7 +14,34 @@
 <main class="wp-block-group">
 	<!-- wp:group {"metadata":{"name":"Container"},"align":"full","layout":{"type":"constrained"}} -->
 	<div class="wp-block-group alignfull">
-		<!-- wp:pattern {"slug":"sorai/page-home-blogging"} /-->
+
+		<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left","contentSize":"880px"}} -->
+		<div class="wp-block-group alignwide">
+			<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.25","fontStyle":"normal","fontWeight":"500"}},"fontSize":"xxxxx-large"} -->
+			<p class="has-xxxxx-large-font-size" style="font-style:normal;font-weight:500;line-height:1.25">
+				<?php esc_html_e( 'Sorai is a WordPress block theme defined by clarity and character—minimalist in form, bold in presence, and built for a focused publishing experience. ', 'sorai' ); ?>
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+		<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:pattern {"slug":"sorai/hidden-post-sticky-home-blogging"} /-->
+
+		<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+		<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+		<div class="wp-block-group alignwide">
+			<!-- wp:pattern {"slug":"sorai/hidden-latest-posts-title"} /-->
+			<!-- wp:pattern {"slug":"sorai/posts-grid-3-columns"} /-->
+		</div>
+		<!-- /wp:group -->
+
 	</div>
 	<!-- /wp:group -->
 </main>
